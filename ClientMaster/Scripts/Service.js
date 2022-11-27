ef46450c9d0bdf6da71aee7def8d4b47eeb3eb81
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-11-27 19:02:24 +0530

    First Commit

---
+++ ClientMaster/Scripts/Service.js
@@ -0,0 +1,33 @@
+﻿function getAllClients() {
+    $.ajax({
+        url: "Service/ClientService.svc/GetAllClients",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            clients = result;
+            bindClientListToTable(clients);
+        }
+    });
+}
+
+
+function addClient(){
+    var client = {
+        "Id": $("#addId").val(),
+        "Code": $("#addCode").val(),
+        "Name": $("#addName").val(),
+        "Exchange": $("#addExchange").val()
+    };
+
+    $.ajax({
+        url: "Service/ClientService.svc/AddClient",
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(client),
+        success: function (result) {
+            getAllClients();
+        }
+    });
+}
+
---
+++ ClientMaster/Service/ClientService.svc.cs
@@ -0,0 +1,34 @@
+﻿using ClientMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ClientMaster.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ClientService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<ClientModel> GetAllClients() // Get All Record
+        {
+            return ClientData.Data;
+        }
+
+       
+        [OperationContract, WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "AddClient", Method = "POST")]
+        public ClientModel AddClient(ClientModel obj) // Add Record
+        {
+            obj.Id = ClientData.Data.Max(x => x.Id) + 1;
+            ClientData.Data.Add(obj);
+            return obj;
+        }
+
+    }
+}
